09903b9043313d3edbdb783c0069755d10d1be3c
Hiren Padariya <hiren.dotnfotech@gmail.com>
2021-08-25 12:30:00 +0530

    header footer implement with design

--- app/components/Footer/index.js
+++ app/components/Footer/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container';
+import footerlogo from '../assets/images/footer-logo.svg';
 
 const Footer = () => (
-  <div className="bg-dark" variant="dark">
-    <Container>
-      <div className="text-white">Footer</div>{' '}
-    </Container>
-  </div>
+  <>
+    <footer className="site-footer">
+      <div className="container">
+        <div className="row align-items-center">
+          <div className="col-md-3">
+            <img src={footerlogo} alt="" />
+          </div>
+          <div className="col-md-6">
+            <p>© 2021 EAB. All Rights Reserved</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </>
 );
 
 export default Footer;
