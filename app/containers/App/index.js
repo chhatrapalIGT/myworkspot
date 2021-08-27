c489dd69a2a991cad6f944db8228591c2eb6b30c
Pinkal Agrawal <pinkal.dotinfotech.com>
2021-08-27 12:57:28 +0530

    added loader and fixed font issue

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -7,30 +7,45 @@
  *
  */
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Switch, Route } from 'react-router-dom';
 // import PropTypes from 'prop-types';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
 
 // import HomePage from 'containers/HomePage/Loadable';
+import Spinner from 'react-bootstrap/Spinner';
 import ProfilePage from '../ProfilePage';
 // import 'bootstrap/dist/css/bootstrap.min.css';
-import GlobalStyle from '../../global-styles';
 import Faq from '../../components/FAQ';
 import Report from '../ReportPage';
 import Boarding from '../onBoardingPage';
+import WorkSpot from '../../components/WorkSpot';
 
-const App = props => (
-  <div>
-    <Switch>
-      <Route exact path="/" component={Boarding} props={props} />
-      <Route exact path="/profile" component={ProfilePage} props={props} />
-      <Route exact path="/faq" component={Faq} />
-      <Route exact path="/report" props={props} component={Report} />
-      <Route component={NotFoundPage} />
-    </Switch>
-    <GlobalStyle />
-  </div>
-);
+const App = props => {
+  const [pageLoading, setPageLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setPageLoading(false);
+    }, 1000);
+  });
+
+  return (
+    <div>
+      {pageLoading ? (
+        <Spinner className="app-spinner" animation="grow" variant="dark" />
+      ) : (
+        <Switch>
+          <Route exact path="/" component={Boarding} props={props} />
+          <Route exact path="/profile" component={ProfilePage} props={props} />
+          <Route exact path="/faq" component={Faq} />
+          <Route exact path="/report" props={props} component={Report} />
+          <Route exact path="/workspot" props={props} component={WorkSpot} />
+          <Route component={NotFoundPage} />
+        </Switch>
+      )}
+    </div>
+  );
+};
 
 export default App;
