d6d8b18db3494e379943f4a530b931f0eeba34d5
viral brahmbhatt <viral.dotinfotech@gmail.com>
2021-09-01 18:47:00 +0530

    Added dragging image functionality

--- app/containers/OfficeMapPage/index.js
+++ app/containers/OfficeMapPage/index.js
@@ -1,54 +1,65 @@
 /* eslint-disable no-nested-ternary */
 /* eslint-disable no-underscore-dangle */
-import React, { Component, createRef } from 'react';
+import React, { Component } from 'react';
 // import PropTypes from 'prop-types';
 import Office from '../../components/OfficeWDC ';
+
+const zoomStep = 1;
+const maxScale = 5;
+const minScale = 1;
+const defaultScale = minScale;
+const defaultRotate = 0;
 
 class OfficeMap extends Component {
   constructor(props) {
     super(props);
-    this.imgRef = createRef();
     this.state = {
-      height: null,
-      width: null,
+      scale: defaultScale,
+      rotate: defaultRotate,
+      version: 0,
     };
   }
 
   handleZoomIn = () => {
-    const height = this.imgRef.current.clientHeight;
-    const width = this.imgRef.current.clientWidth;
-    this.setState({
-      height: height + 30,
-      width: width + 30,
+    this.setState(state => {
+      const newScale = state.scale + zoomStep;
+      return {
+        scale: newScale <= maxScale ? newScale : maxScale,
+      };
     });
   };
 
   handleZoomOut = () => {
-    const height = this.imgRef.current.clientHeight;
-    const width = this.imgRef.current.clientWidth;
-    this.setState({
-      height: height - 20,
-      width: width - 20,
+    this.setState(state => {
+      const newScale = state.scale - zoomStep;
+      return {
+        scale: newScale >= minScale ? newScale : minScale,
+      };
     });
   };
 
   handleDefault = () => {
-    this.setState({
-      height: this.initialHeight,
-      width: this.initialWidth,
-    });
+    this.setState(state => ({
+      scale: defaultScale,
+      rotate: 0,
+      version: state.version + 1,
+    }));
   };
 
   render() {
-    const imgStyle = { height: this.state.height, width: this.state.width };
+    const imgStyle = {
+      transform: `scale(${this.state.scale}) rotate(${this.state.rotate}deg)`,
+    };
+
     return (
       <Office
         imgStyle={imgStyle}
         state={this.state}
-        imgRefData={this.imgRef}
         handleZoomIn={this.handleZoomIn}
         handleZoomOut={this.handleZoomOut}
         handleDefault={this.handleDefault}
+        handleMouseOut={this.handleMouseOut}
+        handleMouseMove={this.handleMouseMove}
       />
     );
   }
